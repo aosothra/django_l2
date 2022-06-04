48f8949611ad688863966d89a13cd5d6c4110e29
Govno <arhdam@yandex.ru>
2022-06-04 21:32:16 +0400

    account for invalid geocoder response

--- locations/models.py
+++ locations/models.py
@@ -24,7 +24,10 @@ def fetch_coordinates(apikey, address):
 
 
 class LocationManager(models.Manager):
+    
     def get_for_addresses(self, addresses: set):
+        '''Get locations from DB; fetch and save any address that is missing'''
+
         locations_from_db = {
             location.address: location for location 
             in self.filter(address__in=addresses)
@@ -33,12 +36,15 @@ class LocationManager(models.Manager):
         new_addresses = addresses.difference(locations_from_db.keys())
 
         for address in new_addresses:
-            lon, lat = fetch_coordinates(settings.GEO_API_KEY, address)
+            coords = fetch_coordinates(settings.GEO_API_KEY, address)
+            lon, lat = coords if coords else (None, None)
+
             location = self.create(
                 address=address,
                 lon=lon,
                 lat=lat
             )
+
             locations_from_db[address] = location
 
         return locations_from_db
