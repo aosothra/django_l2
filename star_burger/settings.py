e80e9147740f21a01b18d12ac51ade2598c2c920
Aosothra <arhdam@yandex.ru>
2022-06-29 20:59:10 +0300

    switch from sqlite to postgres

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -102,9 +102,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-    )
+    'default': dj_database_url.parse(env('POSTGRESQL_DB_URL'))
 }
 
 AUTH_PASSWORD_VALIDATORS = [
